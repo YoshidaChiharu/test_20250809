2e756d140f67732507d6933b01bd435e7b09da67
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2025-08-09 17:00:48 +0900

    テストテキスト全削除

--- src/resources/views/punch.blade.php
+++ src/resources/views/punch.blade.php
@@ -6,7 +6,6 @@
 
 @section('content')
 <div class="container">
-    <div>test_branch_01</div>
     <div class="container__heading">{{$name}}さんお疲れ様です！</div>
     <div class="container__content">
         <div class="message">
